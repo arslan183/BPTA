7979846bb863df46928aab4ee85336fbab3b9528
Mohd Arslan <163635316+arslan183@users.noreply.github.com>
2026-08-12 08:21:52 +0530

    Add files via upload

---
+++ graph_04.py
@@ -0,0 +1,30 @@
+import random
+import networkx as nx
+import matplotlib.pyplot as plt
+
+n = 10
+m = 10
+
+
+
+for m in range(m,46,5):
+    G = nx.Graph()
+
+
+    valid_edges = []
+    i=0
+    while i<m:
+
+        u = random.randint(0, (n-1))
+        v = random.randint(0, (n-1))
+
+        if(u!=v):
+           
+            if (u,v) not in valid_edges and (v,u) not in valid_edges:
+                i=i+1
+                G.add_edge(u , v)
+                valid_edges.append((u, v))
+    
+    nx.draw(G,  with_labels=True)
+    
+    plt.show()
